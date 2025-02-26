b35dd1fbeb557f20b1595241e221d0ce450744a9
adam-lafontaine <adam.lafontiane@gmail.com>
2025-02-26 15:30:41 -0500

    delete unused apps

--- crop/src/pltfm/imgui_sdl2_ogl3/imgui_o.cpp
+++
@@ -1,9 +0,0 @@
-#include "imgui_options.hpp"
-
-#define IMGUI_IMPLEMENTATION
-#define IMGUI_USE_STB_SPRINTF
-
-#include "../../../../libs/imgui/misc/single_file/imgui_single_file.h"
-
-#include "../../../../libs/imgui/backends/imgui_impl_sdl2.cpp"
-#include "../../../../libs/imgui/backends/imgui_impl_opengl3.cpp"
